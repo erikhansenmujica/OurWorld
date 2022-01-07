33a77c3903c6d5d02339765f1674595addfbe971
Erik Hansen <erikhansenmujica@gmail.com>
2022-01-07 11:22:30 +0100

    feat: hexagons visualization

---
+++ src/App.tsx
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import { Ion, Cartesian3, Cartographic, Color, Math } from "cesium";
+import { CesiumMovementEvent, Entity, Viewer, PolygonGraphics } from "resium";
+import { geoToH3, h3ToGeoBoundary, h3IsValid, polyfill } from "h3-js";
+import { resColor, resolutions } from "./utils/colors";
+const { fromDegreesArray, fromDegrees } = Cartesian3;
+const { fromCartesian } = Cartographic;
+
+const App = () => {
+  let viewer: any;
+  const [index, setIndex] = useState<string>("");
+  const [res, setRes] = useState<keyof typeof resColor>(0);
+  const [polygons, setPolygons] = useState<any>([]);
+  const [dot, setDot] = useState<Cartesian3>();
+  const [dotBelongingIndex, setDotBelongingIndex] = useState<string>("");
+  const TRANSPARENCY = 0.4;
+  const material = Color.BLACK.withAlpha(TRANSPARENCY);
+
+  const getBoundary = (index: string) =>
+    h3ToGeoBoundary(index)
+      .map(([lat, lon]) => [lon, lat])
+      .reduce((list, latLon) => [...list, ...latLon], []);
+  const onClick = (data: CesiumMovementEvent) => {
+    if (viewer) {
+      const cartesian = viewer.scene.camera.pickEllipsoid(data.position);
+      if (cartesian) {
+        const zeroIndex = newIndexGenerator(cartesian, 0);
+        if (zeroIndex === index) {
+          const newI = newIndexGenerator(cartesian, res);
+          polygonOnClick(newI, cartesian);
+        } else {
+          setRes(0);
+          setPolygons([]);
+          setIndex(zeroIndex);
+        }
+      }
+    }
+  };
+  const polygonOnClick = (newIndex: string, cartesian: Cartesian3) => {
+    if (
+      Array.isArray(polygons) &&
+      !polygons.includes(newIndex) &&
+      res !== 0 &&
+      res !== 1
+    ) {
+      let num = resolutions[res - 2];
+      let newRes = num;
+      const newI = newIndexGenerator(cartesian, newRes);
+      newIndexGenerator(cartesian, newRes + 1);
+      setPolygons(polyfill(h3ToGeoBoundary(newI), newRes + 1));
+      if (newRes in resColor) {
+        setRes(newRes + 1);
+      }
+    } else if (
+      Array.isArray(polygons) &&
+      !polygons.includes(newIndex) &&
+      res === 1
+    ) {
+      const newI = newIndexGenerator(cartesian, res - 1);
+      setPolygons(polyfill(h3ToGeoBoundary(newI), res));
+    } else if (
+      Array.isArray(polygons) &&
+      polygons.includes(newIndex) &&
+      res !== 0 &&
+      newIndex !== dotBelongingIndex
+    ) {
+    } else {
+      let num = resolutions[res + 1];
+      let newRes = num;
+      newIndexGenerator(cartesian, newRes);
+      setPolygons(polyfill(h3ToGeoBoundary(newIndex), newRes));
+      if (newRes in resColor) {
+        setRes(newRes);
+      }
+    }
+  };
+  function newIndexGenerator(cartesian: Cartesian3, resolution: any) {
+    const { toDegrees } = Math;
+    const { latitude, longitude } = fromCartesian(cartesian);
+    const newPosition = {
+      latitude: toDegrees(latitude),
+      longitude: toDegrees(longitude),
+      height: 0,
+    };
+    const newIndex = geoToH3(
+      newPosition.latitude,
+      newPosition.longitude,
+      resolution
+    );
+    const newDot = fromDegrees(newPosition.longitude, newPosition.latitude);
+    setDot(undefined);
+    setDot(newDot);
+    setDotBelongingIndex(newIndex);
+    return newIndex;
+  }
+  return (
+    <Viewer
+      style={{
+        position: "absolute",
+        left: 0,
+        right: 0,
+        height: "calc( 100%  )",
+      }}
+      timeline={false}
+      ref={(e) => (viewer = e ? e.cesiumElement : undefined)}
+      onClick={onClick}
+    >
+      {h3IsValid(index) && (
+        <Entity
+          description={"H3 Hexagon: {res: 0, h3index: " + index + " }"}
+          polygon={{
+            hierarchy: {
+              positions: fromDegreesArray(getBoundary(index)),
+              holes: [],
+            },
+            material,
+            outline: true,
+            outlineColor: Color.WHITE,
+            height: 0,
+          }}
+        />
+      )}
+      {polygons.length &&
+        polygons.map((p: any, i: number) => (
+          <Entity key={i} description="H3 Hexagon">
+            <PolygonGraphics
+              hierarchy={{
+                positions: fromDegreesArray(getBoundary(p)),
+                holes: [],
+              }}
+              material={resColor[res]}
+              outline={true}
+              outlineColor={Color.WHITE}
+              height={0}
+            ></PolygonGraphics>
+          </Entity>
+        ))}
+      {dot && (
+        <Entity
+          name="dot"
+          description={
+            "Dot, belongs in res " + res + " to h3 index " + dotBelongingIndex
+          }
+          position={dot}
+          selected
+          point={{ pixelSize: 10, color: Color.RED, heightReference: 20000 }}
+        ></Entity>
+      )}
+    </Viewer>
+  );
+};
+
+export default App;
---
+++ src/utils/colors.tsx
@@ -0,0 +1,60 @@
+import { Color } from "cesium";
+const TRANSPARENCY = 0.4;
+
+export const resColor: {
+  0: Color;
+  1: Color;
+  2: Color;
+  3: Color;
+  4: Color;
+  5: Color;
+  6: Color;
+  7: Color;
+  8: Color;
+  9: Color;
+  10: Color;
+  11: Color;
+  12: Color;
+  13: Color;
+  14: Color;
+  15: Color;
+  16: Color;
+} = {
+  0: Color.BLACK.withAlpha(TRANSPARENCY),
+  1: Color.LAWNGREEN.withAlpha(TRANSPARENCY),
+  2: Color.NAVY.withAlpha(TRANSPARENCY),
+  3: Color.OLDLACE.withAlpha(TRANSPARENCY),
+  4: Color.OLIVE.withAlpha(TRANSPARENCY),
+  5: Color.ORANGE.withAlpha(TRANSPARENCY),
+  6: Color.ORANGERED.withAlpha(TRANSPARENCY),
+  7: Color.PALEGOLDENROD.withAlpha(TRANSPARENCY),
+  8: Color.PALEVIOLETRED.withAlpha(TRANSPARENCY),
+  9: Color.PAPAYAWHIP.withAlpha(TRANSPARENCY),
+  10: Color.PEACHPUFF.withAlpha(TRANSPARENCY),
+  11: Color.PERU.withAlpha(TRANSPARENCY),
+  12: Color.PINK.withAlpha(TRANSPARENCY),
+  13: Color.PLUM.withAlpha(TRANSPARENCY),
+  14: Color.POWDERBLUE.withAlpha(TRANSPARENCY),
+  15: Color.PURPLE.withAlpha(TRANSPARENCY),
+  16: Color.RED.withAlpha(TRANSPARENCY),
+};
+
+export const resolutions: any = {
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  11: 11,
+  12: 12,
+  13: 13,
+  14: 14,
+  15: 15,
+  16: 16,
+};
